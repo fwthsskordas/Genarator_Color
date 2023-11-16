e947e4c6c1b4df840cc25b68fc545ca0ec602b6c
fotis <fotis@arx.net>
2023-11-16 16:03:36 +0200

    implement random RGB picker

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,32 @@
 <script setup>
-import { ref } from 'vue';  
+import { ref, reactive } from 'vue';  
 
-const red = ref(0);
-const blue = ref(0);
-const green = ref(0);
+// const color = ref({
+//   red: 0,
+//   blue: 0,
+//   green: 0
+// })
+
+const color = reactive({
+  red: 0,
+  green: 0,
+  blue: 0
+})
+
+const SelectRandom = () => {
+  color.red = Math.floor(Math.random() * 255);
+  color.green = Math.floor(Math.random() * 255);
+  color.blue = Math.floor(Math.random() * 255);
+}
 </script>
 
 <template>
-  <main :style="{backgroundColor: `rgb(${red}, ${green}, ${blue})`}">
+  <main :style="{backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`}">
     <div class="container">
-      <input type="number" v-model="red" placeholder="red">
-      <input type="number" v-model="blue" placeholder="blue">
-      <input type="number" v-model="green" placeholder="green">
+      <input type="number" v-model="color.red" placeholder="red">
+      <input type="number" v-model="color.blue" placeholder="blue">
+      <input type="number" v-model="color.green" placeholder="green">
+      <button @click="SelectRandom"> Select Random Color </button>
     </div>
   </main>
 </template>
